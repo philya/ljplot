967388f57be9f5d7d7effeb9b5da5874685fac20
Philip Olenyk <philya@gmail.com>
2020-11-17 20:44:33 +0200

    Implement chart padding

--- ljplot/chart.py
+++ ljplot/chart.py
@@ -24,6 +24,11 @@ class ChartSettings:
         chart_width=800,
         chart_height=600,
         margin=60,
+        padding=0,
+        padding_top=None,
+        padding_bottom=None,
+        padding_left=None,
+        padding_right=None,
         force_min_value=None,
         label_height=40,
         value_label_x_offset=-0,
@@ -34,6 +39,10 @@ class ChartSettings:
         value_label_format="{:.1%}",
         connect_shape="line", # line or path
         path_curve=0.5,
+
+        title_height = 38,
+
+
     ):
 
         self.chart_width = chart_width
@@ -51,8 +60,13 @@ class ChartSettings:
         self.value_label_format = value_label_format
         self.connect_shape = connect_shape
         self.path_curve = path_curve
+        self.title_height = title_height
 
-
+        self.padding = padding
+        self.padding_top = padding_top
+        self.padding_bottom = padding_bottom
+        self.padding_left = padding_left
+        self.padding_right = padding_right
 
 
 
@@ -61,6 +75,8 @@ class Chart:
     def __init__(self, chart_settings, **kwargs):
 
         self.s = chart_settings
+
+        self._init_padding()
 
         for name, value in kwargs.items():
             setattr(self, name, value)
@@ -72,6 +88,14 @@ class Chart:
 
         self.labels = []
         self.lines = []
+
+
+    def _init_padding(self):
+
+        for d in ['left', 'right', 'top', 'bottom']:
+            an = 'padding_' + d
+            if getattr(self, an) == None:
+                setattr(self, an, self.padding)
 
 
     def __getattr__(self, name):
@@ -102,10 +126,10 @@ class Chart:
 
 
 
-        left = self.margin
-        right = self.chart_width - self.margin
-        top = self.margin # + title_height
-        bottom = self.chart_height - self.margin #- footer_height - self.label_height 
+        left = self.margin + self.padding_left
+        right = self.chart_width - self.margin - self.padding_right
+        top = self.margin + self.padding_top # + title_height
+        bottom = self.chart_height - self.margin - self.padding_bottom #- footer_height - self.label_height 
 
 
         steps = len(self.df)
@@ -153,6 +177,16 @@ class Chart:
                 elements.append(svg_text(step_x + self.value_label_x_offset, bottom - y + self.value_label_y_offset, "value_label", self.value_label_format.format(value)))
 
 
+        if hasattr(self, "title"):
+
+            title_y = self.margin + (self.padding_top + self.title_height) / 2
+            elements.append(svg_text(self.margin, title_y, 'title', self.title))
+        #elements.append(svg_text(margin, margin * 1.7 + title_height , 'subtitle', subtitle))
+
+        #elements.append(svg_text(margin, chart_height - margin, 'signature', signature))
+
+
+
 
         for i, lp in enumerate(line_points):
             color = self.colors[i]
@@ -160,6 +194,9 @@ class Chart:
             #     elements.insert(0, svg_polyline(lp, color, "linechart_line"))
             # elif self.connect_shape == 'path':
             elements.insert(0, svg_polyline_path(lp, color, "linechart_line", step_width * self.path_curve))
+
+            # Another way to smoothen the polyline:
+            # https://medium.com/@francoisromain/smooth-a-svg-path-with-cubic-bezier-curves-e37b49d46c74
 
 
 
